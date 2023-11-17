c7bc8ce7b089ec3a4393d66218b37296ae319a20
Maria Ragone <maria@ragone.net>
2023-11-17 10:30:56 -0500

    more functions added

--- Noble Nudge/Models/NobleNudgeDbContext.cs
+++ Noble Nudge/Models/NobleNudgeDbContext.cs
@@ -27,7 +27,7 @@ public partial class NobleNudgeDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source= noblenudge.database.windows.net;Initial Catalog= NobleNudgeDB; User Id=mangotsunami; Password=7eEka'93:K3ioPJdw(");
+        => optionsBuilder.UseSqlServer(Secret.optbuild);
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
